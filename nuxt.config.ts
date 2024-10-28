6f095878121dce511a883b765ff5e22ae3e5e19e
saco1208 <65397944+sunacodesu@users.noreply.github.com>
2024-10-28 13:29:20 +0900

    change:nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  target: 'static',
+  target: 'server',
   generate: {
     routes: [
       '/', // トップページ
